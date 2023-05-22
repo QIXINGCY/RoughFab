c90c7ea47b1282762116b50692bf73888e569634
QIXINGCY <2815494748@qq.com>
2023-05-22 13:02:26 +0800

    main

--- RoughFab.cpp
+++ RoughFab.cpp
@@ -1,12 +1,42 @@
 ﻿// RoughFab.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
-
-int main()
+#include <igl/opengl/glfw/Viewer.h>
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World and success!\n";
+    // Inline mesh of a cube
+    const Eigen::MatrixXd V = (Eigen::MatrixXd(8, 3) <<
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 1.0,
+        1.0, 0.0, 0.0,
+        1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0,
+        1.0, 1.0, 1.0).finished();
+    const Eigen::MatrixXi F = (Eigen::MatrixXi(12, 3) <<
+        0, 6, 4,
+        0, 2, 6,
+        0, 3, 2,
+        0, 1, 3,
+        2, 7, 6,
+        2, 3, 7,
+        4, 6, 7,
+        4, 7, 5,
+        0, 4, 5,
+        0, 5, 1,
+        1, 5, 7,
+        1, 7, 3).finished();
+
+    // Plot the mesh
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V, F);
+    viewer.data().set_face_based(true);
+    viewer.launch();
 }
+
+
+
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
